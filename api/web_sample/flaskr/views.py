10b4ce4dbb12246213b7ff99c510989f14226e1d
梅宮 <umemiya@MacBook-Pro.local>
2018-08-11 22:51:33 +0900

    flaskrアプリにサンプルapiを搭載しました

--- api/web_sample/flaskr/views.py
+++ api/web_sample/flaskr/views.py
@@ -1,6 +1,8 @@
 from flask import request, redirect, url_for, render_template, flash
+from flask import jsonify
 from flaskr import app, db
 from flaskr.models import Entry
+import random
 
 @app.route('/')
 def show_entries():
@@ -18,3 +20,11 @@ def add_entry():
     db.session.commit()
     flash('新規投稿に成功しました')
     return redirect(url_for('show_entries'))
+
+
+@app.route('/v1/api/randint')
+def random_num():
+    response = {
+        'randnum': random.randint(0,100)
+    }
+    return jsonify(response)
